e26830a8637bab12924c2dba4abab2ccaeb68bf4
Anand Capur <me@anand.io>
2014-02-16 22:37:29 -0500

    Fixed up some production configs for deploying.

--- app/config/database.php
+++ app/config/database.php
@@ -46,41 +46,15 @@ return array(
 
 	'connections' => array(
 
-		'sqlite' => array(
-			'driver'   => 'sqlite',
-			'database' => __DIR__.'/../database/production.sqlite',
-			'prefix'   => '',
-		),
-
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'host'      => 'ReplaceMeOnDeployWithDBHost',
+			'database'  => 'ReplaceMeOnDeployWithDBName',
+			'username'  => 'ReplaceMeOnDeployWithDBUser',
+			'password'  => 'ReplaceMeOnDeployWithDBPass',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
-		),
-
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		),
-
-		'sqlsrv' => array(
-			'driver'   => 'sqlsrv',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'prefix'   => '',
 		),
 
 	),
---
+++ app/config/github.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'client_id' => 'ReplaceMeOnDeployWithGHClientId',
+    'client_secret' => 'ReplaceMeOnDeployWithGHClientSecret',
+    'scope' => 'user:email',
+    'org_token' => 'ReplaceMeOnDeployWithGHOrgToken',
+);
